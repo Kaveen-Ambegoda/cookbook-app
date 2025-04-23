36f4c0351f4e48375f408c4706facf3e2c77fe57
CharukaVithna <clvithana2001@gmail.com>
2025-04-23 16:04:07 +0530

    New REcipe Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex flex-col h-screen">
 
-        <Navbar/>
+        <Navbar />
 
         <div className="flex flex-1">
           <Sidebar />
