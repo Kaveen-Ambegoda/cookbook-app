1511ff7f56a871590a69d6a63885426d330805c5
SandaliSathsarani <125250407+SandaliSathsarani@users.noreply.github.com>
2025-07-13 14:36:26 +0530

    Fixed the issue with navBar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
+"use client";
 import "./globals.css";
 import Sidebar from "./Components/SideBar";
 import Navbar from "./Components/NavBar";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <html lang="en">
       <body className="flex flex-col h-screen">
 
         <div>
-          <Navbar />
+          <Navbar setIsOpen={setIsOpen} />
         </div>
 
         <div className="flex flex-1">
